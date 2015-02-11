abb7a152f77945811968c761bba9d6c98d99bfa0
Thibaut Mattio <thibaut.mattio@gmail.com>
2015-02-11 18:31:28 -0500

    split observers class in a separate module

---
+++ scripts/observer.py
@@ -0,0 +1,51 @@
+"""
+"""
+
+import abc
+
+
+class Observable(object):
+    """Simple Observer class
+    Allow childs class to notify subscribers with notify
+    """
+
+    __metaclass__ = abc.ABCMeta  # ABC class behaves like abstract
+
+    def __init__(self):
+        """Default Constructor
+        Initiat class attributes
+        """
+        self.observers = []
+
+    def attach(self, observer):
+        """Add a new observer to observers list
+        """
+        if observer not in self.observers:
+            self.observers.append(observer)
+
+    def detach(self, observer):
+        """Remove an observer from observers list
+        """
+        try:
+            self.observers.remove(observer)
+        except ValueError:
+            pass
+
+    def notify(self, modifier=None):
+        """Notify all observers that a change occurenced on self
+        """
+        if len(self.observers):
+            for observer in self.observers:
+                if modifier != observer:
+                    observer.update(self)
+
+
+class Observer(object):
+    """
+    """
+
+    __metaclass__ = abc.ABCMeta  # ABC class behaves like abstract
+
+    def update(self):
+        raise NotImplementedError(
+            "Class %s doesn't implement update()" % (self.__class__.__name__))
